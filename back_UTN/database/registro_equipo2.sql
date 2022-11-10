fcc6f24e3708e9345d797c47e1527ced913bd0c4
Lao <lautaro.s.larragueta@gmail.com>
2022-11-10 21:07:03 +0100

    script listo para para mysql workbench

---
+++ back_UTN/database/registro_equipo2.sql
@@ -0,0 +1,30 @@
+
+create database registro_equipo;
+
+
+use registro_equipo;
+
+
+create  table equipo (
+idEquipo int unsigned auto_increment primary key,
+nombre_de_equipo varchar(50),
+ciudad varchar(50), 
+estadio varchar(50),
+fecha_de_creacion date
+
+);
+
+
+select * from equipo;
+
+insert into equipo (idEquipo, nombre_de_equipo, ciudad, estadio, fecha_de_creacion)
+values (null, "monteweed", " kilometro 24 ", "san rafael", "2010-05-28"); 
+
+update equipo set nombre_de_equipo = " San rafael " where idEquipo= 9;
+
+delete from equipo WHERE idEquipo=10;
+
+
+
+
+
